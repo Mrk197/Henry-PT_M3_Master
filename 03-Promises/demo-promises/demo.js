dc8f2e74a4be6798021935c3c650f0651f9bd885
Mrk197 <mirkaalamilla97@gmail.com>
2023-03-01 15:14:44 -0600

    demo-promises axios-fetch

--- 03-Promises/demo-promises/demo.js
+++ 03-Promises/demo-promises/demo.js
@@ -9,7 +9,9 @@ const axios = require('axios');
     )
  */
 
-const miPromesa = new Promise((resolve, reject) => {
+
+//CREANDO MI PROMESA 
+/* const miPromesa = new Promise((resolve, reject) => {
     if (3 < 5) {
         resolve('Mi promesa se resolvió, todo OK')
     }
@@ -25,6 +27,24 @@ miPromesa
         //error => console.log(error
     )
     .then(response => console.log(response + ' segundo then'))//SE PUEDE TENER ENCADENACIÓN SE THEN - se va pasando la info
-    .catch(err => console.log(err)) //UNICO CATCH
+    .catch(err => console.log(err)) //UNICO CATCH */
 
 //console.log(miPromesa);
+
+/* let username = [];
+
+let response = axios('https://jsonplaceholder.typicode.com/users')  //por default hace petición GET 
+        .then( response => response.data)
+        .then(data => data.map(user => user.username))
+        .catch(err => console.log(err))
+    
+console.log(response); */
+
+
+
+//PROMESA CON FETCH
+fetch('https://jsonplaceholder.typicode.com/users')  //cuando se usa del lado del servidor FETCH se tiene que instalar
+    .then( response => response.json())
+    .then( response => response.data)
+    .then(data => data.map(user => user.username))
+    .catch(err => console.log(err))
