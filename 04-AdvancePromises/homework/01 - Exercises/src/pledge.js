b1b829463da5ef8ecc58c04cf2fcf1d86015fe08
Mrk197 <mirkaalamilla97@gmail.com>
2023-03-07 07:47:16 -0600

    changes

--- 04-AdvancePromises/homework/01 - Exercises/src/pledge.js
+++ 04-AdvancePromises/homework/01 - Exercises/src/pledge.js
@@ -7,12 +7,55 @@ function $Promise(executor) {
     if (typeof executor !== "function") {
         throw new TypeError("executor no es una function")
     }
-    else{
+    this._state = "pending";
+    this._value = undefined;
+    this._handlerGroups = [];
 
-    }
-    
+    executor(
+        (data)=>{this._internalResolve(data)},
+        (data)=>{this._internalReject(data)}
+        /* this._internalResolve.bind(this), this._internalReject.bind(this) */
+    );
 }
 
+$Promise.prototype._internalResolve = function (data) {
+    if (this._state === "pending") {
+        this._state = "fulfilled";
+        this._value = data;
+        this._callHandlers();
+    }    
+};
+
+$Promise.prototype._internalReject = function (data) {
+    if (this._state === "pending") {
+        this._state = "rejected";
+        this._value = data;
+        this._callHandlers();
+    }   
+};
+
+$Promise.prototype.then = function (successCb, errorCb) {
+    this._handlerGroups.push({
+        successCb: typeof successCb === "function" ? successCb : false, 
+        errorCb: typeof errorCb === "function" ? errorCb: false,
+    })
+
+    this._callHandlers();
+};
+
+$Promise.prototype._callHandlers = function () {
+    if (this._state !== "pending") {
+        while (this._handlerGroups.length) {
+            const handlers = this._handlerGroups.shift();
+
+            if (this._state === "fulfilled") {
+                if (handlers.successCb) {
+                    handlers.successCb(this._value);
+                }
+            }
+        }
+    }
+}
 
 module.exports = $Promise;
 /*-------------------------------------------------------
--- 05-Express/demoClase/index.js
+++ 05-Express/demoClase/index.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const server = express();
+
+server.use("/", (req,res, next) =>{
+    console.log("estamos pasando por este middleware");
+    next();
+});
+
+server.use(morgan("dev"));
 
 server.get("/", (req, res) => {
     res.send("Todo ok, ruta /")
@@ -8,6 +16,10 @@ server.get("/", (req, res) => {
 
 server.get("/users", (req, res) => {
     res.send("Estamos en la ruta users")
+});
+
+server.get("/posts", (req, res) => {
+    res.send("Estamos en la ruta posts")
 });
 
 server.listen("3001", () => {
